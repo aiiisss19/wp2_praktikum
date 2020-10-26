5da83915f0c81261afd2df859d6a5f00f243e8d4
aiiisss19 <aisatulmutoharoh@gmail.com>
2020-10-26 17:58:09 +0700

    wp2_praktikum

---
+++ Folder 1/pustaka_ais/application/controllers/perkenalan.php
@@ -0,0 +1,10 @@
+<?php
+class perkenalan extends CI_Controller{
+    public function index() {
+        echo "<h1> perkenalan</h1>";
+        echo "Aisatul Mutoharoh<br>
+                saya tinggal di Tegal<br>
+                saya ingin tinggal di Antartika";
+    }
+
+}
---
+++ Folder 1/pustaka_ais/application/views/v_about.php
@@ -0,0 +1,22 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Aisatul</li>
+        <li>Nama Belakang : Mutoharoh</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Pangkah Tegal</li> </ul>
+    
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+    <li>Bekasi</li></ul>
+
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Nonton film</li>
+        <li>Minum Kopi</li>
+    </ul>
+</section>
